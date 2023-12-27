68e33055a733b5296ea98222d64fdf6aed45769e
Lewy Seiden <lewy@seiden.us>
2023-12-26 16:35:54 -0800

    except BuildConstants.java from spotless check on build

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "503ea589d7f676dead191c2d62897eacd95e3e1d";
-  public static final String GIT_DATE = "2023-12-26 16:21:43 PST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "45f6b27a02ddcb28800cb6bfdc0d13c08a2422fb";
+  public static final String GIT_DATE = "2023-12-26 16:31:32 PST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-12-26 16:28:40 PST";
-  public static final long BUILD_UNIX_TIME = 1703636920080L;
+  public static final String BUILD_DATE = "2023-12-26 16:35:28 PST";
+  public static final long BUILD_UNIX_TIME = 1703637328963L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
