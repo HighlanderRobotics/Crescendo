8ec3ef2078dc2f720ee37bf825f7de183dd5882e
Lewis-Seiden <lewy@seiden.us>
2024-01-13 11:54:40 -0800

    rename init check and make properly check for init errors

--- src/test/java/frc/InitTests.java
+++ src/test/java/frc/InitTests.java
@@ -4,23 +4,25 @@
 
 package frc;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import edu.wpi.first.hal.HAL;
 import frc.robot.Robot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /** Test robot init. */
 public class InitTests {
-    @BeforeEach
-    private void setup() {
-        HAL.initialize(500, 0);
-    }
-
-    @Test
-    private void initTest() {
-        var robot = new Robot();
-        robot.startCompetition();
-        robot.close();
-    }
+  @BeforeEach
+  private void setup() {
+    HAL.initialize(500, 0);
   }
+
+  @Test
+  protected void initializeRobotDoesntThrow() {
+    assertDoesNotThrow(
+        () -> {
+          new Robot().close();
+        });
+  }
+}
