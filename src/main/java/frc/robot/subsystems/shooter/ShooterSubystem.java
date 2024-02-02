545ab9e8ccf1b7a1e6ef34be4968230c3bbfea36
Lewis-Seiden <lewy@seiden.us>
2024-02-01 17:08:28 -0800

    -reworked ShooterSubsystem

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubystem.java
@@ -1,19 +1,38 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
+import org.littletonrobotics.junction.Logger;
 
 public class ShooterSubystem extends SubsystemBase {
-  ShooterIO PivotIO;
-  ShooterIOInputs PivotInputs;
+  ShooterIO io;
+  ShooterIOInputsAutoLogged inputs;
 
-  public ShooterSubystem(ShooterIO PivotIO) {
-    this.PivotIO = PivotIO;
-    PivotInputs = new ShooterIOInputs();
+  public ShooterSubystem(ShooterIO pivotIO) {
+    this.io = pivotIO;
+    inputs = new ShooterIOInputsAutoLogged();
   }
 
   @Override
   public void periodic() {
-    PivotInputs = PivotIO.updateInputs();
+    inputs = io.updateInputs();
+    Logger.processInputs("Shooter", inputs);
+  }
+
+  public Command runStateCmd(Rotation2d rotation, double rps) {
+    return this.run(
+        () -> {
+          io.setFlywheelVelocity(rps);
+          io.setPivotSetpoint(rotation);
+        });
+  }
+
+  public Command runFlywheelvoltageCmd(Rotation2d rotation, double voltage) {
+    return this.run(
+        () -> {
+          io.setFlywheelVoltage(voltage);
+          io.setPivotSetpoint(new Rotation2d());
+        });
   }
 }
