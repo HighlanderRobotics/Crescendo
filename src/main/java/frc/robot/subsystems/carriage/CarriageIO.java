b976258bc5793f49a19837291ba263454339dc39
Lewis-Seiden <lewy@seiden.us>
2024-02-06 23:58:07 -0800

    added final to member variables, inverteddigitalinput subclasses digitalinput

--- src/main/java/frc/robot/subsystems/carriage/CarriageIO.java
+++ src/main/java/frc/robot/subsystems/carriage/CarriageIO.java
@@ -17,7 +17,7 @@ public interface CarriageIO {
     public boolean beambreak = false;
   }
 
-  public default void updateInputs(CarriageIOInputsAutoLogged inputs) {}
+  public default void updateInputs(final CarriageIOInputsAutoLogged inputs) {}
 
-  public default void setVoltage(double volts) {}
+  public default void setVoltage(final double volts) {}
 }
