55392ad9401d801f5278bfcdff486314734efc9b
Lewis-Seiden <lewy@seiden.us>
2024-02-13 18:05:43 -0800

    added sysid to elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -8,13 +8,23 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import static edu.wpi.first.units.Units.Volts;
+
 import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix6.SignalLogger;
 
 /** Slanted cascading elevator */
 public class ElevatorSubsystem extends SubsystemBase {
@@ -24,12 +34,16 @@ public class ElevatorSubsystem extends SubsystemBase {
   public static final double DRUM_RADIUS_METERS = Units.inchesToMeters(1.751 / 2.0);
   public static final Rotation2d ELEVATOR_ANGLE = Rotation2d.fromDegrees(65.0);
 
+  public static final double MAX_EXTENSION_METERS = Units.inchesToMeters(31.3);
+
   public static final double CLIMB_EXTENSION_METERS = 0.6;
   public static final double TRAP_EXTENSION_METERS = 0.9;
   public static final double AMP_EXTENSION_METERS = 0.6;
 
   private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
   private final ElevatorIO io;
+  
+  private final SysIdRoutine elevatorRoutine;
 
   // For dashboard
   private final Mechanism2d mech2d = new Mechanism2d(3.0, Units.feetToMeters(4.0));
@@ -43,6 +57,17 @@ public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem(ElevatorIO io) {
     this.io = io;
+
+    elevatorRoutine =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null, // Default ramp rate is acceptable
+                Volts.of(4),
+                null, // Default timeout is acceptable
+                // Log state with Phoenix SignalLogger class
+                (state) -> SignalLogger.writeString("state", state.toString())),
+            new SysIdRoutine.Mechanism(
+                (Measure<Voltage> volts) -> io.setVoltage(volts.in(Volts)), null, this));
 
     root.append(carriage);
   }
@@ -64,6 +89,26 @@ public class ElevatorSubsystem extends SubsystemBase {
           io.setTarget(meters.getAsDouble());
           Logger.recordOutput("Elevator/Setpoint", meters.getAsDouble());
         });
+  }
+
+  public Command runSysidCmd() {
+    return Commands.sequence(
+        this.runOnce(() -> SignalLogger.start()),
+        // Stop when we get close to max to avoid hitting hard stop
+        elevatorRoutine.quasistatic(Direction.kForward).until(() -> inputs.elevatorPositionMeters > MAX_EXTENSION_METERS - 0.2),
+        this.runOnce(() -> io.setVoltage(0.0)),
+        Commands.waitSeconds(1.0),
+        // Stop when we get close to max to avoid hitting hard stop
+        elevatorRoutine.quasistatic(Direction.kReverse).until(() -> inputs.elevatorPositionMeters < 0.2),
+        this.runOnce(() -> io.setVoltage(0.0)),
+        Commands.waitSeconds(1.0),
+        // Stop when we get close to max to avoid hitting hard stop
+        elevatorRoutine.dynamic(Direction.kForward).until(() -> inputs.elevatorPositionMeters > MAX_EXTENSION_METERS - 0.2),
+        this.runOnce(() -> io.setVoltage(0.0)),
+        Commands.waitSeconds(1.0),
+        // Stop when we get close to max to avoid hitting hard stop
+        elevatorRoutine.dynamic(Direction.kReverse).until(() -> inputs.elevatorPositionMeters < 0.2),
+        this.runOnce(() -> SignalLogger.stop()));
   }
 
   public Pose3d getCarriagePose() {
