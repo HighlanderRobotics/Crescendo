d9e07dfd85d5932abec73d73eb2642fef0b8ae9b
Awesomeplayer165 <dev@jacob2.dev>
2024-02-18 22:52:35 -0800

    removed segment .1.traj ending files (140/280)

---
+++ src/main/java/frc/robot/utils/dynamicauto/RemoveTrajSplits.py
@@ -0,0 +1,22 @@
+import json
+import os
+import urllib.parse
+
+folder_path = "src/main/java/frc/robot/utils/dynamicauto/deploy/choreo"
+files_in_folder = os.listdir(folder_path)
+
+print("Files in deploy/choreo folder:")
+print(len(files_in_folder) / 2)
+
+repeatFiles = []
+
+for file in files_in_folder:
+    if file.endswith(".1.traj"):
+        repeatFiles.append(file)
+
+print(f"Removing {len(repeatFiles)} files...")
+
+for file in repeatFiles:
+    os.remove(os.path.join(folder_path, file))
+
+print("Files removed.")
