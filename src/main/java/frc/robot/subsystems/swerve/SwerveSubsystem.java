b339bfdf6d6b0029c9c7b1fc447918ca29c25053
team computer <lewy@seiden.us>
2024-02-21 17:53:32 -0800

    merged bring up into bring up vision

--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -194,7 +194,7 @@ public class SwerveSubsystem extends SubsystemBase {
           -0.040501622476628085); // TODO find!!
   public static final VisionConstants leftCamConstants =
       new VisionConstants(
-          "Left Camera",
+          "Left_Camera",
           new Transform3d(
               new Translation3d(
                   Units.inchesToMeters(10.386),
@@ -203,17 +203,17 @@ public class SwerveSubsystem extends SubsystemBase {
               new Rotation3d(0, -0.490, -Math.PI * 7 / 6)), // TODO check
           LEFT_CAMERA_MATRIX,
           LEFT_DIST_COEFFS);
-  public static final VisionConstants rightCamConstants =
-      new VisionConstants(
-          "Right Camera",
-          new Transform3d(
-              new Translation3d(
-                  Units.inchesToMeters(-10.597),
-                  Units.inchesToMeters(-10.143),
-                  Units.inchesToMeters(-7.384)),
-              new Rotation3d(0, -0.490, Math.PI * 2 / 3)), // TODO check
-          RIGHT_CAMERA_MATRIX_OPT,
-          RIGHT_DIST_COEFFS_OPT);
+  // public static final VisionConstants rightCamConstants =
+  //     new VisionConstants(
+  //         "Right Camera",
+  //         new Transform3d(
+  //             new Translation3d(
+  //                 Units.inchesToMeters(-10.597),
+  //                 Units.inchesToMeters(-10.143),
+  //                 Units.inchesToMeters(-7.384)),
+  //             new Rotation3d(0, -0.490, Math.PI * 2 / 3)), // TODO check
+  //         RIGHT_CAMERA_MATRIX_OPT,
+  //         RIGHT_DIST_COEFFS_OPT);
   private SwerveDriveOdometry odometry;
 
   private final SysIdRoutine moduleSteerRoutine;
@@ -331,7 +331,9 @@ public class SwerveSubsystem extends SubsystemBase {
    * @return The array of vision IOs.
    */
   public static VisionIO[] createRealCameras() {
-    return new VisionIO[] {new VisionIOReal(leftCamConstants), new VisionIOReal(rightCamConstants)};
+    return new VisionIO[] {new VisionIOReal(leftCamConstants)
+      // , new VisionIOReal(rightCamConstants)
+    };
   }
 
   /**
@@ -340,7 +342,9 @@ public class SwerveSubsystem extends SubsystemBase {
    * @return The array of vision IOs.
    */
   public static VisionIO[] createSimCameras() {
-    return new VisionIO[] {new VisionIOSim(leftCamConstants), new VisionIOSim(rightCamConstants)};
+    return new VisionIO[] {new VisionIOSim(leftCamConstants)
+      // , new VisionIOSim(rightCamConstants)
+    };
   }
 
   public void periodic() {
@@ -588,14 +592,15 @@ public class SwerveSubsystem extends SubsystemBase {
   }
 
   public static VisionConstants[] getCameraConstants() {
-    return new VisionConstants[] {leftCamConstants, rightCamConstants};
+    return new VisionConstants[] {leftCamConstants
+      // , rightCamConstants
+    };
   }
 
   /** Returns the module positions (turn angles and drive velocities) for all of the modules. */
-  @AutoLogOutput(key = "SwerveStates/Measured")
   private SwerveModulePosition[] getModulePositions() {
-    SwerveModulePosition[] states = new SwerveModulePosition[4];
-    Arrays.stream(modules).map(Module::getPosition).toArray();
+    SwerveModulePosition[] states =
+        Arrays.stream(modules).map(Module::getPosition).toArray(SwerveModulePosition[]::new);
     return states;
   }
 
