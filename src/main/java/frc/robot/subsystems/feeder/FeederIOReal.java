a74728123c0665871bf3922547e78b90ec5c56e4
Lewis-Seiden <lewy@seiden.us>
2024-02-29 14:40:03 -0800

    mark feeder beambreaks as private final

--- src/main/java/frc/robot/subsystems/feeder/FeederIOReal.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederIOReal.java
@@ -17,8 +17,8 @@ import frc.robot.utils.components.InvertedDigitalInput;
 public class FeederIOReal implements FeederIO {
   private final TalonFX motor = new TalonFX(13, "canivore");
 
-  InvertedDigitalInput firstBeambreak = new InvertedDigitalInput(0);
-  InvertedDigitalInput lastBeambreak = new InvertedDigitalInput(1);
+  private final InvertedDigitalInput firstBeambreak = new InvertedDigitalInput(0);
+  private final InvertedDigitalInput lastBeambreak = new InvertedDigitalInput(1);
 
   private final StatusSignal<Double> velocity = motor.getVelocity();
   private final StatusSignal<Double> voltage = motor.getMotorVoltage();
