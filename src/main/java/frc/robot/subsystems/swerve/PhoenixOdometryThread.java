f3c33ae0ddd9f55a0bfa39e485a36dbf225bbe0a
Lewis-Seiden <Lewis-Seiden@users.noreply.github.com>
2024-03-01 23:07:11 +0000

    run spotless

--- src/main/java/frc/robot/subsystems/swerve/PhoenixOdometryThread.java
+++ src/main/java/frc/robot/subsystems/swerve/PhoenixOdometryThread.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
