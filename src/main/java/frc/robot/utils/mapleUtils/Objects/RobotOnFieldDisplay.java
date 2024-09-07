ee90b90900692d59d73e4c242e107400a19dd061
Lewis-Seiden <lewy@seiden.us>
2024-09-06 17:25:23 -0700

    imported maple utils for swerve sim

---
+++ src/main/java/frc/robot/utils/mapleUtils/Objects/RobotOnFieldDisplay.java
@@ -0,0 +1,14 @@
+package frc.robot.utils.mapleUtils.Objects;
+
+import frc.robot.utils.mapleUtils.CompetitionFieldVisualizer;
+
+/**
+ * displays a robot on field
+ * note that the main robot also inherits this class,
+ * but it will not be displayed in "Robots" Pose3d array
+ * it will be displayed as "Robot" and with a single Pose2d
+ * */
+public interface RobotOnFieldDisplay extends CompetitionFieldVisualizer.Object2dOnFieldDisplay {
+    @Override
+    default String getTypeName() {return "Robots";}
+}
---
+++ src/main/java/frc/robot/utils/mapleUtils/RobotOnFieldDisplay.java
@@ -0,0 +1,12 @@
+package frc.robot.utils.mapleUtils;
+
+/**
+ * displays a robot on field note that the main robot also inherits this class, but it will not be
+ * displayed in "Robots" Pose3d array it will be displayed as "Robot" and with a single Pose2d
+ */
+public interface RobotOnFieldDisplay extends CompetitionFieldVisualizer.Object2dOnFieldDisplay {
+  @Override
+  default String getTypeName() {
+    return "Robots";
+  }
+}
