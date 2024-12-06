c254978cfae151864535ce93a5b12cadfcbaa52e
SCool62 <samthecool300@icloud.com>
2024-12-05 17:30:52 -0800

    Overloaded with array tolerences and boolean check values

--- src/main/java/frc/robot/utils/PitChecks.java
+++ src/main/java/frc/robot/utils/PitChecks.java
@@ -7,6 +7,8 @@ package frc.robot.utils;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
@@ -38,5 +40,52 @@ public class PitChecks {
                         }
                       }
                     }));
+  }
+  public static Command runCheck(
+          Supplier<double[]> expectedValues,
+          Supplier<double[]> tolerance,
+          Supplier<double[]> outputValues,
+          Command cmd,
+          double time,
+          String name) {
+      return cmd.withTimeout(time * 2)
+              .beforeStarting(() -> Logger.recordOutput(name, "ffff00"))
+              .alongWith(
+                      Commands.waitSeconds(time)
+                              .finallyDo(
+                                      () -> {
+                                          for (int i = 0; i < expectedValues.get().length; i++) { // assumes it's the same length
+                                              if (MathUtil.isNear(
+                                                      expectedValues.get()[i],
+                                                      outputValues.get()[i],
+                                                      tolerance.get()[i])) {
+                                                  Logger.recordOutput(name, "00ff00");
+                                              } else {
+                                                  Logger.recordOutput(name, "ff0000");
+                                              }
+                                            }
+                                      }));
+  }
+
+  public static Command runCheck(
+          BooleanSupplier expected,
+          BooleanSupplier output,
+          Command cmd,
+          double time,
+          String name) {
+      return cmd.withTimeout(time * 2)
+              .beforeStarting(() -> Logger.recordOutput(name, "ffff00"))
+              .alongWith(
+                      Commands.waitSeconds(time)
+                              .finallyDo(
+                                      () -> {
+                                          if (expected.getAsBoolean() == output.getAsBoolean()) {
+                                              Logger.recordOutput(name, "00ff00");
+                                          } else {
+                                              Logger.recordOutput(name, "ff0000");
+                                          }
+                                      }
+                              )
+              );
   }
 }
